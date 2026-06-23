49a9c3d26ec63a13a5bc6e5dee6b59f750a191d3
Kimseonghyeon <90964556+dev-seonghyeon@users.noreply.github.com>
2026-06-23 17:31:19 +0900

    feat : regs, inst textfile reading and saving

--- riscvsim.c
+++ riscvsim.c
@@ -2,12 +2,10 @@
  * title : riscvsim.c
  * author : Kimseonghyeon
  * description : simulate risc-v instructions
- *
  */
 
 // TODO: 과거버전에서 첫 번째 문장을 실행하면 어떻게 되나 ??
-// getline()함수를 쓰기 위한 매크로
-// glibc버전마다 선언 매크로가 다름(glibc 2.10 기준)
+// getline() -glibc버전마다 선언 매크로가 다름(glibc 2.10 기준)-
 #define _POSIX_C_SOURCE 200809L
 #define _GNU_SOURCE
 
@@ -16,32 +14,30 @@
 #include <stdlib.h>
 #include <string.h>
 
-// 레지스터의 값(64비트)를 담는 배열
 // 인덱스가 곧 레지스터 번호임
 static uint64_t register_file[32] = {(uint64_t)0};
 
-// 명령어 저장용 구조체
+// 사용하지 않는 필드는 MUX 구현에서 선택해야함
 struct instructions {
-
-	// TODO : 이거 signed, unsigned구분해서 써야함... 어쩌지...
-	// R, I, S에 따라 구조체를 따로 작성하기? 단순함과 멀다....
-	uint32_t bit_31_25 : 7; // f3 or imm
-	uint32_t bit_24_20 : 5; // rs2 or imm
-	uint32_t bit_19_15 : 5; // rs1
-	uint32_t bit_14_12 : 3; // funct3
-	uint32_t bit_11_7 : 5;	// rd or imm
-	uint32_t bit_6_0 : 7;	// opcode
-
+	int funct7_or_imm;
+	int rs2;
+	int rs1;
+	int funct3;
+	int rd;
+	int opcode;
 	struct instructions *next;
 };
 
-// 수평선 출력 유틸
+// 어차피 한 번씩만 지나가니까 전역으로 선언함 (beq도 없고)
+struct instructions *inst_head = NULL;
+struct instructions *inst_tail = NULL;
+
 static void print_horizon()
 {
 	printf("==========================================\n");
+	return;
 }
 
-// 레지스터파일 디버깅용 출력 유틸
 static void print_register_file()
 {
 	printf("\n");
@@ -53,82 +49,204 @@ static void print_register_file()
 	}
 	print_horizon();
 	printf("\n");
+	return;
 }
 
-int main(int argc, char *argv[])
+static void read_regs_txt(char *file_name)
 {
-	// command line parameter validation
-	if (argc < 3) {
-		printf("usage ...\n");
-		exit(EXIT_FAILURE);
-	}
-
-	print_register_file();
-
-	FILE *regs = NULL;
+	FILE *regs_file = NULL;
 	char *line = NULL;
 	size_t size = 0;
 	ssize_t nread;
+	int n;
+	int register_number;
+	uint64_t register_value;
 
-	regs = fopen(argv[1], "r");
-	if (!regs) {
+	regs_file = fopen(file_name, "r");
+	if (!regs_file) {
 		printf("regs.txt file open error\n");
 		exit(EXIT_FAILURE);
 	}
 
-	while ((nread = getline(&line, &size, regs)) != -1) {
-
+	while ((nread = getline(&line, &size, regs_file)) != -1) {
 		// printf("%s", line);
-
-		int sscanf_output_n = 0;
-		int register_number;
-		uint64_t register_value;
-
-		sscanf_output_n = sscanf(line, "X%d %*s 0x%" SCNx64,
-					 &register_number, &register_value);
-
-		if (sscanf_output_n == -1) {
+		n = sscanf(line, "X%d %*s 0x%" SCNx64, &register_number, &register_value);
+		if (n == -1) {
 			perror("sscanf error");
+			exit(EXIT_FAILURE);
 		}
 		// printf("reg num : %d\n", register_number);
 		// printf("reg value : %" PRIx64 "\n", register_value);
 
 		// x0는 0고정됨
 		if (register_number != 0)
+			// 초기 값이 모두 0일 때 성립
+			// 그냥 대입해도 되겠는데? ,,,
 			register_file[register_number] |= register_value;
 	}
-	print_register_file();
 
-	FILE *insts;
+	free(line);
+	fclose(regs_file);
+	return;
+}
+
+static void read_inst_txt(char *file_name)
+{
+	FILE *insts_file = NULL;
 	char *inst_line = NULL;
-	size_t size2 = 0;
-	ssize_t nread2;
+	size_t size = 0;
+	ssize_t nread;
+	int n;
+	char inst_name[5];
 
-	insts = fopen(argv[2], "r");
-	if (!insts) {
+	insts_file = fopen(file_name, "r");
+	if (!insts_file) {
 		perror("inst file open error\n");
 		exit(EXIT_FAILURE);
 	}
 
-	while ((nread2 = getline(&inst_line, &size2, insts)) != -1) {
-		int ssanf_output;
+	while ((nread = getline(&inst_line, &size, insts_file)) != -1) {
 
-		// sscanf로 instruction 이름만 추출
-		// 명령어에 따라 opcode, funct3, funct7값 명시적으로 할당
-		// 명령어에 따라 파싱할 rs1, rs2, rd, imm값 지정
-		// 숫자로 변환된 최종 inst를 linked list에 저장
-		// struct inst가 있어야함
-		// linked list 위한 구조체 필요함(struct inst 넣고, 포인터
-		// 넣고?)
+		n = sscanf(inst_line, "%s", inst_name);
+		if (n == -1) {
+			perror("sscanf error");
+			exit(EXIT_FAILURE);
+		}
+		// printf("%s\n", inst_name);
+
+		struct instructions *inst_node =
+		    (struct instructions *)malloc(sizeof(struct instructions));
+
+		inst_node->next = NULL;
+
+		if (!inst_head) {
+			inst_head = inst_node;
+			inst_tail = inst_node;
+		} else {
+			inst_tail->next = inst_node;
+			inst_tail = inst_node;
+		}
+
+		if (strcmp(inst_name, "ld") == 0) {
+			inst_node->funct3 = 0b011;
+			inst_node->opcode = 0b0000011;
+
+			sscanf(inst_line, "%*s x%d %*s %d (x%d %*s", &inst_node->rd,
+			       &inst_node->funct7_or_imm, &inst_node->rs1);
+
+			continue;
+		}
+		if (strcmp(inst_name, "sd") == 0) {
+			inst_node->funct3 = 0b011;
+			inst_node->opcode = 0b0100011;
+
+			sscanf(inst_line, "%*s x%d %*s %d (x%d %*s", &inst_node->rs2,
+			       &inst_node->funct7_or_imm, &inst_node->rs1);
+
+			continue;
+		}
+
+		// TODO : sscanf로직 만들기
+		if ('i' == inst_name[strlen(inst_name) - 1]) {
+			inst_node->opcode = 0b0010011; // 나머지 I-type
+
+			sscanf(inst_line, "%*s x%d %*s x%d %*s %d", &inst_node->rd, &inst_node->rs1,
+			       &inst_node->funct7_or_imm);
+		} else {
+			inst_node->opcode = 0b0110011; // R-type
+
+			sscanf(inst_line, "%*s x%d %*s x%d %*s x%d", &inst_node->rd,
+			       &inst_node->rs1, &inst_node->rs2);
+
+			if (strcmp(inst_name, "sub") == 0)
+				inst_node->funct7_or_imm = 0b0100000; // funct7
+			else
+				inst_node->funct7_or_imm = 0b0000000; // funct7
+		}
+
+		if ((strstr(inst_name, "add") != NULL) || (strstr(inst_name, "sub") != NULL)) {
+			inst_node->funct3 = 0b000;
+			continue;
+		}
+		if (strstr(inst_name, "sll") != NULL) {
+			inst_node->funct3 = 0b001;
+			continue;
+		}
+		if (strstr(inst_name, "srl") != NULL) {
+			inst_node->funct3 = 0b101;
+			continue;
+		}
+		if (strstr(inst_name, "xor") != NULL) {
+			// or보다 먼저 나와야함!!
+			inst_node->funct3 = 0b100;
+			continue;
+		}
+		if (strstr(inst_name, "and") != NULL) {
+			inst_node->funct3 = 0b111;
+			continue;
+		}
+		if (strstr(inst_name, "or") != NULL) {
+			inst_node->funct3 = 0b110;
+			continue;
+		}
+	}
+
+	free(inst_line);
+	fclose(insts_file);
+	return;
+}
+
+static void print_inst_linked_list(struct instructions *head)
+{
+	struct instructions *ptr = head;
+
+	printf("f7|imm  rs2   rs1   f3   rd    opcode\n");
+	while (ptr != NULL) {
+		printf("%07b ", ptr->funct7_or_imm);
+		printf("%05b ", ptr->rs2);
+		printf("%05b ", ptr->rs1);
+		printf("%03b ", ptr->funct3);
+		printf("%05b ", ptr->rd);
+		printf("%07b ", ptr->opcode);
+		printf("\n");
+		ptr = ptr->next;
+	}
+	return;
+}
+
+int main(int argc, char *argv[])
+{
+	// command line parameter validation
+	if (argc < 3) {
+		printf("usage ... 2 files\n");
+		exit(EXIT_FAILURE);
+	}
+
+	// print_register_file();
+	read_regs_txt(argv[1]);
+	// print_register_file();
+
+	read_inst_txt(argv[2]);
+	// linked list traversal
+	print_inst_linked_list(inst_head);
+	// ===========main loop========
+	while (1) {
+		// clock rising
+		// IF -> ID -> EX -> MEM -> WB
+		// pipeline register write
+		// WB write
+		//
+
+		// clock falling
+		// IF -> ID -> EX -> MEM -> WB
+		// pipeline register read
+		// ID read
+
+		// 언제 종료함 ???
+		break;
 	}
 
 	// =========자원해제==========
-	free(line);
-	fclose(regs);
-
-	free(inst_line);
-	fclose(insts);
-	// free() 파싱한 명령어 문자열
 	// free() instructions linked list 로직
 
 	return 0;
--- test/sign_extension.c
+++ test/sign_extension.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 struct mini {
-	int32_t a : 3;
+	uint32_t a : 12;
 };
 
 int main()
@@ -10,13 +10,14 @@ int main()
 	struct mini m;
 	m.a = -3;
 
-	int64_t b = m.a;
+	// 명시적인 수동 sign extension
+	// 끝까지 갔다가 다시 돌리면
+	// 젤 끝 비트로 채워짐
+	int64_t b = ((int64_t)m.a << 52) >> 52;
 
 	int64_t c = (int64_t)3;
 	b = b + c;
-	printf("0x%" PRIX64 "\n", b);
+	printf("0x%" PRIx64 "\n", b);
 
 	return 0;
-
-
 }
